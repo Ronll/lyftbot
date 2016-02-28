ec9c06eae698686efb6fd8497d32404da1854c40
Ron Lapushner <lapushnerron@gmail.com>
2016-02-28 15:09:06 -0800

    created app

--- index.js
+++ index.js
@@ -50,7 +50,6 @@ app.post('/', function(req, res){
   }
 
   request(optionsETA, function (error, response, body) {
-      var body = JSON.parse(body);
       console.log(body, 'herererererr out of loop 1');
       etaEstimates = body.eta_estimates; 
       for( var i = 0; i < etaEstimates.length ; i++){
@@ -64,7 +63,6 @@ app.post('/', function(req, res){
 	return a.eta_estimates - b.eta_estimates
       })
       request(optionsCOST, function(error, response, body){
-	body = JSON.parse(body);
 	console.log('in request 2');
 	if(error) console.log(error);
 	costEstimates = body.cost_estimates; 
