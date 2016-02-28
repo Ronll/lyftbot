87a80690f88cf7cf0569070049d5c2b82cad43ef
Ron Lapushner <lapushnerron@gmail.com>
2016-02-28 12:18:54 -0800

    started server

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 app.post('/', function(req, res){
+  console.log(req.body);
   res.end();  
 })
+
+app.listen(443 , function () {
+    console.log('Ready');
+});
